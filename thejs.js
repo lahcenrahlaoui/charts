ecad13996663181b18fee2fcea182611a39d4cd5
lahcenrahlaoui <lahcen.rahlaoui@univ-tiaret.dz>
2021-04-15 17:36:41 +0100

    after making the toggle navbar working

--- thejs.js
+++ thejs.js
@@ -1,7 +1,41 @@
 const hamburger= document.querySelector(".hamburger");
 const navlinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
+
+var nav_bool= false;
+
 hamburger.addEventListener('click',()=>{
-  navlinks.classList.toggle("open");
-  
+  navlinks.classList.add("open");
+  console.log("hamburger");
+
 });
+
+const mybody= document.querySelector(".mybody");
+
+mybody.addEventListener('click',()=>{
+  const test_navlinks = document.querySelector(".nav-links");
+
+  if((test_navlinks.classList.contains('open')) && (nav_bool)){
+    navlinks.classList.remove("open");
+    nav_bool = !nav_bool;
+  }else{
+    nav_bool = !nav_bool;
+  }
+
+});
+
+
+
+/*
+jQuery(".hamburger").on("click", function (event) {
+  jQuery(this).toggleClass('open');
+  event.stopPropagation();
+});
+jQuery(".sub-menu").on("click", function (event) {
+  event.stopPropagation();
+});
+jQuery(".mybody").on("click", function () {
+  jQuery('#menu-button').prop('checked', true); //method of choice: our method uses the input checkbox method as fallback.
+  jQuery('#menu-button').removeClass('on');
+});
+*/
